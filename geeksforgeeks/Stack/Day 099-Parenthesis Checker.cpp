4a859de78b261771881d5d77861f5e703288f8f1
Ashutosh Tagore <ashutoshtagore9@gmail.com>
2026-03-26 22:04:50 +0530

    Add Day99: Solution

---
+++ geeksforgeeks/Stack/Day 099-Parenthesis Checker.cpp
@@ -0,0 +1,37 @@
+/* Que.99) Parenthesis Checker   */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        stack<char> st;
+        
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+                
+                char top = st.top();
+                
+                if ((c == ')' && top == '(') || 
+                    (c == '}' && top == '{') || 
+                    (c == ']' && top == '[')) {
+                    
+                    st.pop(); 
+                } 
+                else {
+                    return false; 
+                }
+            }
+        }
+        
+        return st.empty();
+    }
+};

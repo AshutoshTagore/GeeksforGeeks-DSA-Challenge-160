bd9709b0841b691b30e0e3f258e247b228515219
Ashutosh Tagore <ashutoshtagore9@gmail.com>
2026-04-20 22:27:02 +0530

    Add Day120: Solution

---
+++ geeksforgeeks/Dynamic Programming/Day 120-Coin Change (Count Ways).cpp
@@ -0,0 +1,28 @@
+/* Coin Change (Count Ways) */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countWays(int coins[], int n, int sum) {
+    int dp[sum + 1] = {0};
+    dp[0] = 1;
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = coins[i]; j <= sum; j++) {
+            dp[j] += dp[j - coins[i]];
+        }
+    }
+    
+    return dp[sum];
+}
+
+int main() {
+    int coins[] = {1, 2, 3};
+    int n = sizeof(coins) / sizeof(coins[0]);
+    int sum = 4;
+    
+    cout << countWays(coins, n, sum) << endl;
+    
+    return 0;
+}
